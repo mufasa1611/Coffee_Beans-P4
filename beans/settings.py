191ea6f6644d471565dc5a3a3987a6ed195d552f
Mohamed <mufasa1611@gmail.com>
2024-03-20 11:28:27 +0100

    Add  Whitenoise configuration

--- beans/settings.py
+++ beans/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Define the absolute path to the directory where Django will collect static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
