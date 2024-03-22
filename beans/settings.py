9b6774d90226a252c92c1db46f583adb0534b5eb
Mohamed <mufasa1611@gmail.com>
2024-03-23 00:59:26 +0100

    Add database to elephant sql server

--- beans/settings.py
+++ beans/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,10 +117,7 @@ WSGI_APPLICATION = 'beans.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
