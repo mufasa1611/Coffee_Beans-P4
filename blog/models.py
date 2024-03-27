0305485af6a49c28d61e30ec09e913ff9cf34b97
Mohamed <mufasa1611@gmail.com>
2024-03-27 07:07:39 +0100

    Update Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Post model
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
@@ -24,3 +23,18 @@ class Post(models.Model):
           return f"{self.title} | written by {self.author}"
 
 
+# Comment  model .
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+    
