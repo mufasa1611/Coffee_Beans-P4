5a96b8b0a059e5b21983d9446d9d1b1515b3387c
Mohamed <mufasa1611@gmail.com>
2024-03-27 07:02:16 +0100

    Update Post model and URL configuration

--- blog/models.py
+++ blog/models.py
@@ -1,29 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Post model
 STATUS = ((0, "Draft"), (1, "Published"))
 
-    # Post model .
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
              User, on_delete=models.CASCADE,
-             related_name="blog_posts")
+             related_name="posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+ 
+
     class Meta:
           ordering = ["-created_on"]
 
     def __str__(self):
           return f"{self.title} | written by {self.author}"
-      
-     
-      
-
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Blog
-
+from .views import PostList
 
 urlpatterns = [
-    path('', Blog.as_view(), name='home')
-    ]
+    path('', PostList.as_view(), name='home'),
+    
+]
