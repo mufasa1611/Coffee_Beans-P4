56f1df5c68d6470cd913817d6b66aa4b28abde06
Mohamed <mufasa1611@gmail.com>
2024-04-14 16:39:16 +0200

    Rename 'body' field to 'content' in database

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
@@ -35,5 +35,5 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment {self.content} by {self.author}"
     
