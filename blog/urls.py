f750450568590e8b2261ddd3a143f32695b74f4e
Mohamed <mufasa1611@gmail.com>
2024-10-07 18:45:51 +0200

    Add contact page and URL route

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,10 @@ from . import views
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('<slug:slug>/', post_detail, name='post_detail'),
-    
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-    
-        path('<slug:slug>/delete_comment/<int:comment_id>/', 
-             views.comment_delete, name='comment_delete')
+    path('<slug:slug>/delete_comment/<int:comment_id>/', 
+         views.comment_delete, name='comment_delete')
     ]
