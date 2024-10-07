019aa3ba26e373958c9dca80647d0bfa21dd2ca8
Mohamed <mufasa1611@gmail.com>
2024-10-07 17:35:40 +0200

    Add ContactMessage model

--- blog/models.py
+++ blog/models.py
@@ -55,3 +55,13 @@ class Comment(models.Model):
     def save(self, *args, **kwargs):
         self.content = clean_nbsp(self.content)
         super().save(*args, **kwargs) 
+ 
+ # toring the contact us messages.       
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"       
