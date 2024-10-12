e41ef4fb796c73b4ef13326fa022c9d4d07b1813
Mohamed <mufasa1611@gmail.com>
2024-10-12 18:40:07 +0200

    Modify models.py

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 import re
@@ -67,12 +68,12 @@ class ContactMessage(models.Model):
         return f"Message from {self.name} ({self.email})"       
     
 class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     added_at = models.DateTimeField(auto_now_add=True)
-
+    
     class Meta:
-        unique_together = ('user', 'post')
+        unique_together = ('user', 'post')            
 
     def __str__(self):
-        return f"{self.user.username} - {self.post.title}"
+        return f"{self.user.username} favorites {self.post.title}"
