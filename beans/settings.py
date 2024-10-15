f578d1aefdec77918f01711237e25c5537e5ca84
Mohamed <mufasa1611@gmail.com>
2024-10-15 08:37:40 +0200

    update readme file

--- beans/settings.py
+++ beans/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
